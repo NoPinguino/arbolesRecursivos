3bcb992faec21161f36454deba91356119b58c39
NoPinguino <misael.delamorena@gmail.com>
2025-11-05 16:28:48 +0100

    Comentarios y prebas añadidas.

--- src/TresOrdenes/Main.java
+++ src/TresOrdenes/Main.java
@@ -18,21 +18,30 @@ public class Main {
 
         System.out.println("\nRecorridos del árbol:");
 
+        /*
+        Imprimo árbol original:
+         */
         arbol.recorridoPostorden(); // Uso PostOrder() para las pruebas, usa el recorrido que más te guste :)
         System.out.println();
 
-        if(!arbol.buscarNodo(15))
+        /*
+        Busco 15 en el árbol, si no está, lo añado. Imprimo el árbol al terminar.
+         */
+        if(!arbol.buscarNodo(15)) {
             System.out.println("El valor 15 NO se ha encontrado.");
-        arbol.recorridoPostorden();
-        System.out.println();
-
-        arbol.insertarNodo(15);
-        if(arbol.buscarNodo(15))
+            System.out.println("Insertando 15 en el árbol...");
+            arbol.insertarNodo(15);
+        } else {
             System.out.println("El valor 15 se ha encontrado.");
+        }
         arbol.recorridoPostorden();
         System.out.println();
 
-        arbol.delete(12);
+        /*
+        Elimino 15 del árbol y hago una última impresión.
+         */
+        System.out.println("Eliminando 15 de el árbol...");
+        arbol.delete(15);
         arbol.recorridoPostorden(); //Como puedes ver, elimina el valor 12 del árbol.
     }
 }
